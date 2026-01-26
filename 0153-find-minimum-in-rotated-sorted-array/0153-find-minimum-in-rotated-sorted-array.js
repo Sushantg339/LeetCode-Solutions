51e1611ac093ed16d9294658e485728e209bf0aa
Sushant Gupta <157876866+Sushantg339@users.noreply.github.com>
2026-01-27 01:00:29 +0530

    Time: 0 ms (100%), Space: 53.7 MB (69.91%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let s = 0
+    let e = nums.length-1
+    let ans = -1
+    while(s<e){
+        let mid = Math.floor(s + (e-s)/2)
+        if(nums[mid] > nums[e]){
+            s = mid+1
+        }else{
+            e = mid
+        }
+    }
+
+    return nums[s]
+};
