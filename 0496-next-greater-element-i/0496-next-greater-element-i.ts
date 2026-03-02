c6ad2c36853c089c3610e2bd8aeef1d9b818d916
Sushant Gupta <157876866+Sushantg339@users.noreply.github.com>
2026-03-02 23:04:20 +0530

    Time: 7 ms (26.3%), Space: 59.2 MB (21.54%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.ts
+++ 0496-next-greater-element-i/0496-next-greater-element-i.ts
@@ -1,20 +1,15 @@
 function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-    let ans: number[] = []
-    for(let i=0 ; i<nums1.length ; i++){
-        let j=0 
-        for(; j<nums2.length ; j++){
-            if(nums1[i] === nums2[j]) break
+    let map = new Map<number, number>()
+    let st: number[] = []
+
+    for(let i=nums2.length-1 ; i>=0 ; i--){
+        while(st.length && st[st.length-1] <= nums2[i]){
+            st.pop()
         }
 
-        let elem = -1
-        for(let k=j+1 ; k<nums2.length ; k++){
-            if(nums2[k] > nums1[i]){
-                elem = nums2[k]
-                break
-            }
-        }
-        ans.push(elem)
+        map.set(nums2[i], st.length ? st[st.length-1] : -1)
+        st.push(nums2[i])
     }
 
-    return ans
+    return nums1.map(num => map.get(num))
 };
