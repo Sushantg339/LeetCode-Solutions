834959c5fff6f8c6395969a5acb089044f6f4165
Sushant Gupta <157876866+Sushantg339@users.noreply.github.com>
2026-04-07 14:34:48 +0530

    Time: 0 ms (100%), Space: 11 MB (44.01%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,19 +11,20 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* node, vector<int>& ans){
-        if(node == nullptr) return;
+    void postorder(TreeNode* node, vector<int>& ans){
+        if(node == nullptr){
+            return;
+        }
 
-        solve(node->left, ans);
-        solve(node->right, ans);
-
+        postorder(node->left, ans);
+        postorder(node->right, ans);
         ans.push_back(node->val);
     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
+        if(root == nullptr) return ans;
 
-        solve(root, ans);
-
+        postorder(root, ans);
         return ans;
     }
 };
