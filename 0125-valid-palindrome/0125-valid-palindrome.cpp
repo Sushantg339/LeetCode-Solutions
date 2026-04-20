93a2fdc7cde0318f07ad516562f452a8b58abbd9
Sushant Gupta <157876866+Sushantg339@users.noreply.github.com>
2026-04-20 13:37:39 +0530

    Time: 4 ms (15.42%), Space: 9.8 MB (77.54%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string str) {
+        int s = 0, e = str.size()-1;
+
+        while(s<e){
+            if(!isalnum(str[s])){
+                s++;
+                continue;
+            }
+
+            if(!isalnum(str[e])){
+                e--;
+                continue;
+            }
+
+            if(tolower(str[s]) != tolower(str[e])) return false;
+
+            s++;
+            e--;
+        }
+        return true;
+    }
+};
